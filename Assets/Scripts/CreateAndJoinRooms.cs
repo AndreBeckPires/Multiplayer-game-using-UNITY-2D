33fd8abaf76de0e43b71a833865637b3453efbe1
André Pires <andrebeckpires@gmail.com>
2022-09-18 11:59:04 -0300

    server setup

---
+++ Assets/Scripts/CreateAndJoinRooms.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    public InputField createInput;
+    public InputField joinInput;
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom("salatestemockada");
+
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom("salatestemockada");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("SampleScene");
+    }
+}
