5be94f51e2890a1ff972c285a5dacdc193c5c9ec
Júlia Reichert <andrebeckpires@gmail.com>
2022-10-16 20:26:04 -0300

    online movement

--- Assets/Scripts/movementscript.cs
+++ Assets/Scripts/movementscript.cs
@@ -2,39 +2,51 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class movementscript : MonoBehaviour
 {
     // Start is called before the first frame update
    public float speed = 10.5f; 
 
+   PhotonView view;
+
     // Update is called once per frame
    
+    private void Start(){
+        view = GetComponent<PhotonView>();
+    }
     void Update()
     {
-        Vector3 pos = transform.position;
-        Vector3 mousePosition = (Camera.main.ScreenToWorldPoint(Input.mousePosition));
-        Vector2 direction = (mousePosition - transform.position).normalized;
+
+        if(view.IsMine)
+        {
+            Vector3 pos = transform.position;
+            Vector3 mousePosition = (Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            Vector2 direction = (mousePosition - transform.position).normalized;
 
 
-        if (Input.GetKey("w")) 
-        {
-            pos.y += speed * Time.deltaTime;
-        }
-        if (Input.GetKey("s")) 
-        {
-            pos.y -= speed * Time.deltaTime;
-        }
-        if (Input.GetKey("d")) 
-        {
-            pos.x += speed * Time.deltaTime;
-        }
-        if (Input.GetKey("a")) 
-        {
-            pos.x -= speed * Time.deltaTime;
-        } 
 
-        transform.position = pos;
-        transform.up = direction;
+            if (Input.GetKey("w")) 
+            {
+                pos.y += speed * Time.deltaTime;
+            }
+            if (Input.GetKey("s")) 
+            {
+                pos.y -= speed * Time.deltaTime;
+            }
+            if (Input.GetKey("d")) 
+            {
+                pos.x += speed * Time.deltaTime;
+            }
+            if (Input.GetKey("a")) 
+            {
+                pos.x -= speed * Time.deltaTime;
+            } 
+
+            transform.position = pos;
+            transform.up = direction;
+            }
+       
     }
 }
